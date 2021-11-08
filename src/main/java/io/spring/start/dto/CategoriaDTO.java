10b154375b7c32f058b78d6cd6388a894fb8d253
Matielo J Geronimo <matielo.dev@gmail.com>
2021-11-08 20:37:11 -0300

    validação sintática Bean Validation

--- src/main/java/io/spring/start/dto/CategoriaDTO.java
+++ src/main/java/io/spring/start/dto/CategoriaDTO.java
@@ -2,33 +2,43 @@ package io.spring.start.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
+
 import io.spring.start.domain.Categoria;
 
 public class CategoriaDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	
 	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O Tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
-	
-	public CategoriaDTO(){
-		
+
+	public CategoriaDTO() {
+
 	}
+
 	public CategoriaDTO(Categoria obj) {
-		id= obj.getId();
-		nome=obj.getNome();
+		id = obj.getId();
+		nome = obj.getNome();
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 }
