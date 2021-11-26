8c29363f4a0c9490f1849e2fd95e644238dc6758
Matielo J Geronimo <matielo.dev@gmail.com>
2021-11-26 01:22:25 -0300

    Salvando URL da imagem em Cliente

--- src/main/java/io/spring/start/services/ClienteService.java
+++ src/main/java/io/spring/start/services/ClienteService.java
@@ -115,7 +115,19 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+
 	public URI uploadProfilePicture(MultipartFile multipartFile) {
-		return s3Service.uploadFile(multipartFile);
-	} 
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+
+		URI uri = s3Service.uploadFile(multipartFile);
+
+		Cliente cli = repo.findByEmail(user.getUsername());
+		cli.setImageUrl(uri.toString());
+		repo.save(cli);
+
+		return uri;
+	}
 }
