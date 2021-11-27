f0938164f958f3791712f4055201e9575f18a50e
Matielo J Geronimo <matielo.dev@gmail.com>
2021-11-27 14:44:34 -0300

    Ajustando tamanho da imagem

--- src/main/java/io/spring/start/services/ClienteService.java
+++ src/main/java/io/spring/start/services/ClienteService.java
@@ -52,6 +52,9 @@ public class ClienteService {
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
 
+	@Value("${img.profile.size}")
+	private Integer size;
+	
 	// não precisa intanciar , pois o Spring o faz com o @Autowired
 	// ocorre uma injeção de dependência
 
@@ -131,6 +134,8 @@ public class ClienteService {
 		}
 
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
